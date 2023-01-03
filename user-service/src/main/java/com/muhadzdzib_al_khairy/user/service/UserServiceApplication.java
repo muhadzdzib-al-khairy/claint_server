b17314b51a5b9044fee106cc0099cde47f6c5bde
muhadzdzib al khairy <113493493+muhadzdzib-al-khairy@users.noreply.github.com>
2023-01-04 00:43:20 +0700

    Add files via upload

---
+++ user-service/src/main/java/com/muhadzdzib_al_khairy/user/service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.muhadzdzib_al_khairy.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
